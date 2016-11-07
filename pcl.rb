f6c95b93e14cddb60444465710fbe4d6521be6b5
Koji Noshita <noshita@NishizonoShinji-2.local>
2016-11-08 03:19:24 +0900

    pcl: add surface_on_nurbs option

--- pcl.rb
+++ pcl.rb
@@ -17,6 +17,7 @@ class Pcl < Formula
   option "with-examples", "Build pcl examples."
   option "without-tools", "Build without tools."
   option "without-apps", "Build without apps."
+  option "with-surface_on_nurbs", "Build with surface_on_nurbs."
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
@@ -115,6 +116,12 @@ class Pcl < Formula
       args << "-DOPENNI_INCLUDE_DIR=#{Formula["openni"].opt_include}/ni"
     else
       args << "-DCMAKE_DISABLE_FIND_PACKAGE_OpenNI:BOOL=TRUE"
+    end
+
+    if build.with? "surface_on_nurbs"
+      args << "-DBUILD_surface_on_nurbs:BOOL=ON"
+    else
+      args << "-DBUILD_surface_on_nurbs:BOOL=OFF"
     end
 
     args << "-DCMAKE_DISABLE_FIND_PACKAGE_VTK:BOOL=TRUE" if build.without? "vtk"
